70e1e39e8036ebbd33bb22bca8e9590b0c309b15
Gabriel andersson santiago <gabriel.andersson@hotmail.com>
2016-01-27 17:36:24 +0100

    Task 8: Switched to discrete controller and plotted for same sampling times as in Task 7.

--- pid_design.m
+++ pid_design.m
@@ -12,8 +12,9 @@ g = 9.82;
 Tau = 1/alpha1*sqrt(2*tank_h10/g);
 K = 60*beta*Tau;
 Gamma = alpha1^2/alpha2^2;
-Ts = 1;
+
 %TOOOYOOTOTOTO
+Ts = 1;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Continuous Control design
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -37,7 +38,7 @@ omega0 = 0.2;
 [K, Ti, Td, N] = polePlacePID(chi, omega0, zeta,Tau,Gamma,K);
 C = K + K/(Ti*s) + (K*Td*N*s)/(s+N);
 F = C; % Transfer function for the controller
-sim('tanks')
+% sim('tanks')
 % plot(lowerTank_result.Time,lowerTank_result.Data)
 OpenLoop = F*G;
 [Gm,Pm,Wgm,Wpm] = margin(OpenLoop) ;
@@ -47,14 +48,15 @@ disp(['Crossover Frequency : ' , num2str(Wpm)])
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Ts = 1; % Sampling time
 
-% samplingTimes = [1,2,4,8,6];
+samplingTimes = [1,2,4,8,16];
 % for i = 1:length(samplingTimes)
 %     Ts = samplingTimes(i);
+% %     Fd = c2d(F,Ts,'zoh');    
 %     sim('tanks')
 %     figure
 %     plot(lowerTank_result.Time,lowerTank_result.Data)
-%     title(['Zero-Order Hold Sampling time: ',num2str(Ts)])
-% %     print(['ZeroOrder_Sampling_',num2str(Ts)],'-dpng')
+%     title(['Discrete Controller Sampling time: ',num2str(Ts)])
+% %      print(['Discrete_ctrl_Sampling',num2str(Ts)],'-dpng')
 % end
 
 % Discretize the continous controller, save it in state space form
@@ -63,6 +65,8 @@ Ts = 1; % Sampling time
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Discrete Control design
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%To discretize the continous controller use c2d(Controller,Ts,'zoh')
 
-Gd = 1; % Sampled system model
-Fd = 1; % Transfer function for discrete designed controller
+
+Gd = G; % Sampled system model
+Fd = c2d(F,Ts,'zoh'); % Transfer function for discrete designed controller
