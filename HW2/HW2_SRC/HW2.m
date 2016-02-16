8ba46c7b7b5880ef5a85bb13245e0725de1cc2d2
Spookz0r <gabriel.andersson@hotmail.com>
2016-02-16 15:28:50 +0100

    EX3 plots with labels

--- HW2/HW2_SRC/HW2.m
+++ HW2/HW2_SRC/HW2.m
@@ -1,20 +1,31 @@
 close all, clear all, clc
+ex1 = 1;
+
 sim('inv_pend_three')
 figure
-
-title('hej')
 subplot(3,1,1)
 plot(pend1)
+title('Response for Pendulum 1')
+ylabel('Angle')
 subplot(3,1,2)
 plot(pend2)
+title('Response for Pendulum 2')
+ylabel('Angle')
 subplot(3,1,3)
 plot(pend3)
-
-
+ylabel('Angle')
+title('Response for Pendulum 3')
 figure
 subplot(3,1,1)
 plot(ctrl1)
+
+title('Control performance for Pendulum 1')
 subplot(3,1,2)
 plot(ctrl2)
+title('Control performance for Pendulum 2')
 subplot(3,1,3)
 plot(ctrl3)
+title('Control performance for Pendulum 3')
+
+
+
