1e1ccbbdca13a7f546e3c2be5ce5f18091e80924
Spookz0r <gabriel.andersson@hotmail.com>
2016-02-16 16:53:49 +0100

    Ex5 pictures

--- HW2/HW2_SRC/HW2.m
+++ HW2/HW2_SRC/HW2.m
@@ -5,27 +5,27 @@ sim('inv_pend_three')
 figure
 subplot(3,1,1)
 plot(pend1)
-title('Response for Pendulum 1, execution time 6 ms')
+title('Response for Pendulum 1, execution time 10 ms')
 ylabel('Angle')
 subplot(3,1,2)
 plot(pend2)
-title('Response for Pendulum 2, execution time 6 ms')
+title('Response for Pendulum 2, execution time 10 ms')
 ylabel('Angle')
 subplot(3,1,3)
 plot(pend3)
 ylabel('Angle')
-title('Response for Pendulum 3, execution time 6 ms')
+title('Response for Pendulum 3, execution time 10 ms')
 figure
 subplot(3,1,1)
 plot(ctrl1)
 
-title('Control performance for Pendulum 1, execution time 6 ms')
+title('Control performance for Pendulum 1, execution time 10 ms')
 subplot(3,1,2)
 plot(ctrl2)
-title('Control performance for Pendulum 2, execution time 6 ms')
+title('Control performance for Pendulum 2, execution time 10 ms')
 subplot(3,1,3)
 plot(ctrl3)
-title('Control performance for Pendulum 3, execution time 6 ms')
+title('Control performance for Pendulum 3, execution time 10 ms')
 
 
 
