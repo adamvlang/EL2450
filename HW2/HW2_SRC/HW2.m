1c1d8b8e1f94f3a975e5aede56399b3d794414ba
Spookz0r <gabriel.andersson@hotmail.com>
2016-02-16 16:24:45 +0100

    EX3: schedule for 6ms

--- HW2/HW2_SRC/HW2.m
+++ HW2/HW2_SRC/HW2.m
@@ -5,30 +5,27 @@ sim('inv_pend_three')
 figure
 subplot(3,1,1)
 plot(pend1)
-title('Response for Pendulum 1')
+title('Response for Pendulum 1, execution time 6 ms')
 ylabel('Angle')
 subplot(3,1,2)
 plot(pend2)
-title('Response for Pendulum 2')
+title('Response for Pendulum 2, execution time 6 ms')
 ylabel('Angle')
 subplot(3,1,3)
 plot(pend3)
 ylabel('Angle')
-title('Response for Pendulum 3')
+title('Response for Pendulum 3, execution time 6 ms')
 figure
 subplot(3,1,1)
 plot(ctrl1)
 
-title('Control performance for Pendulum 1')
+title('Control performance for Pendulum 1, execution time 6 ms')
 subplot(3,1,2)
 plot(ctrl2)
-title('Control performance for Pendulum 2')
+title('Control performance for Pendulum 2, execution time 6 ms')
 subplot(3,1,3)
 plot(ctrl3)
-title('Control performance for Pendulum 3')
+title('Control performance for Pendulum 3, execution time 6 ms')
 
-figure
-plot(Schedule)
-title('Schedule')
 
 
